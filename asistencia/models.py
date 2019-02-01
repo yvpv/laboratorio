ffe3c565557ecc2680784f6336e446b108310c9f
yvpv <yulennisperales@gmail.com>
2019-02-01 17:08:40 -0400

    versión 2

--- asistencia/models.py
+++ asistencia/models.py
@@ -5,8 +5,9 @@ class Asistencia(models.Model):
     profesor = models.CharField(max_length= 200)
     materia = models.CharField(max_length= 200)
     seccion = models.IntegerField
-    dia = models.DateTimeField('Fecha asistencia')
-    asistio = models.BooleanField
+    entrada = models.TimeField('Hora Entrada')
+    salida = models.TimeField('Hora Salida')
+    asistio = models.BooleanField(default = True)
 
     def __str__(self):
         return '%s %s' % (self.profesor, self.materia)
--- inventario/models.py
+++ inventario/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 
 class Inventario(models.Model):
+
+    active = 'actv'
+    deactive = 'dctv'
+
+    opciones = (
+        (active, 'Activo'),
+        (deactive, 'Inactivo',)
+    )
+
     descripcion = models.CharField(max_length = 200)
     marca = models.CharField(max_length = 200)
     caracteristicas = models.TextField(max_length = 500)
-    estado = models.CharField(max_length = 200)
+    observacion = models.TextField(max_length = 300)
+    serial = models.CharField(max_length= 100)
+    fecha_ingreso = models.DateTimeField('Fecha de Ingreso')
+    estado = models.CharField(max_length=4,choices=opciones, default= active,)
 
     def __str__(self):
         return '%s %s' % (self.descripcion, self.marca)
